4d7a563162d7b345d0d8bb45c2d770640c957e1c
toyop-GsF9 <a3saki4yume@gmail.com>
2023-09-19 16:48:35 +0900

    slideshow追加

--- components/index.js
+++ components/index.js
@@ -2,3 +2,4 @@ export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as FundWallet } from "./FundWallet";
 export { default as MyNFTContainer } from './MyNFTContainer';
+export { default as Slideshow } from './Slideshow';
