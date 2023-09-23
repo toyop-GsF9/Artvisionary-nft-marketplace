647805d461b392175fac66f507147f7fbbe563e0
Yugo <satoyugo1022@gmail.com>
2023-09-24 02:51:15 +0900

    set up database connection and express server

---
+++ backend/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json()); 
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev")); 
+}
+
+module.exports = app;
