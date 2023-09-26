57eb7f9e54e505d2163315f7db9faaddbabee0eb
toyop-GsF9 <a3saki4yume@gmail.com>
2023-09-26 20:15:25 +0900

    QRコード変更

--- pages/square.js
+++ pages/square.js
@@ -4,8 +4,10 @@ import ContractABI from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplac
 import { toast } from "react-toastify";
 import axios from "axios";
 import { ethers } from "ethers";
+// import { truncateEthAddress } from "../utils/truncAddress";
 import { useRouter } from "next/router";
 import { QRCodeComponent } from "../components";
+
 
 const mainURL = `https://arweave.net/`;
 
@@ -16,21 +18,25 @@ export default function Square() {
   const router = useRouter();
 
   const handleGoBack = () => {
-    router.push("/playlist");
+    router.push('/playlist');
   };
+
 
   const getContract = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum);
-    return new ethers.Contract(
+
+    let contract = new ethers.Contract(
       process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
       ContractABI.abi,
       provider
     );
+    return contract;
   };
 
   const getNfts = async () => {
     try {
       const contract = await getContract();
+
       const data = await contract.fetchMarketItems();
 
       const items = await Promise.all(
@@ -38,7 +44,8 @@ export default function Square() {
           const tokenURI = await contract.tokenURI(i.tokenId);
           const meta = await axios.get(mainURL + tokenURI);
           let price = ethers.utils.formatUnits(i.price.toString(), "ether");
-          return {
+
+          let item = {
             price,
             tokenId: i.tokenId.toNumber(),
             seller: i.seller,
@@ -48,10 +55,11 @@ export default function Square() {
             description: meta.data.description,
             tokenURI,
           };
+          return item;
         })
       );
-
       // setNfts(items);
+
       setNfts([...items].reverse());
       setLoading(true);
     } catch (error) {
@@ -60,21 +68,33 @@ export default function Square() {
     }
   };
 
-  // useEffect(() => {
-  //   getNfts();
-  // }, []);
+
 
   useEffect(() => {
+    getNfts();
+  }, []);
+
+
+
+  // useEffectの内部で画像のスライドショーの動作を設定
+  useEffect(() => {
+    // loadingがtrueでない場合、またはnftsが空の場合はタイマーを設定しない
     if (!loading || nfts.length === 0) return;
+
+    // 3秒ごとにnextSlide関数を呼び出すタイマーを設定
     const timerId = setInterval(() => {
       nextSlide();
-    }, 5000);
+    }, 5000); // 3000ミリ秒 = 3秒
+
+    // useEffectのクリーンアップ関数でタイマーをクリア
     return () => {
       clearInterval(timerId);
     };
-  }, [loading, nfts, currentSlideIndex]);
+  }, [loading, nfts, currentSlideIndex]); // loading, nfts, currentSlideIndex のいずれかが変更されたときに再度設定
 
-  if (loading)
+
+
+  if (!loading)
     return (
       <div className="w-full h-screen flex flex-col items-center justify-center font-body">
         <img src="/logo.png" alt="logo" className="h-[160px] animate-bounce" />
@@ -82,14 +102,22 @@ export default function Square() {
       </div>
     );
 
-  const currentNft = nfts[currentSlideIndex];
-  const nftDetailURL = `https://artvisionary02.vercel.app/nft-details?price=${currentNft?.price
-    }&tokenId=${currentNft?.tokenId}&seller=${currentNft?.seller}&owner=${currentNft?.owner
-    }&image=${encodeURIComponent(currentNft?.image)}&name=${encodeURIComponent(
-      currentNft?.name
-    )}&description=${encodeURIComponent(
-      currentNft?.description
-    )}&tokenURI=${encodeURIComponent(currentNft?.tokenURI)}`;
+
+
+
+
+  const nextSlide = () => {
+    setCurrentSlideIndex((prevIndex) => (prevIndex + 1) % nfts.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlideIndex((prevIndex) => (prevIndex - 1 + nfts.length) % nfts.length);
+  };
+
+  // NFTの詳細ページへのURLを取得
+  const nftDetailURL = `https://artvisionary02.vercel.app/nft-details?price=${nfts[currentSlideIndex]?.price}&tokenId=${nfts[currentSlideIndex]?.tokenId}&seller=${nfts[currentSlideIndex]?.seller}&owner=${nfts[currentSlideIndex]?.owner}&image=${encodeURIComponent(nfts[currentSlideIndex]?.image)}&name=${encodeURIComponent(nfts[currentSlideIndex]?.name)}&description=${encodeURIComponent(nfts[currentSlideIndex]?.description)}&tokenURI=${encodeURIComponent(nfts[currentSlideIndex]?.tokenURI)}`;
+  const nftDetailURL2 = `https://artvisionary02.vercel.app/dashboard`;
+
 
   return (
     <>
@@ -100,8 +128,8 @@ export default function Square() {
       <div className="w-full h-[930px] max-w-[1440px] flex items-center justify-center bg-black relative">
         <div className="w-[852px] h-[852px] bg-[#1a1a1a] flex items-center justify-center relative ">
           <img
-            src={mainURL + currentNft?.image}
-            alt={currentNft?.name}
+            src={mainURL + nfts[currentSlideIndex]?.image}
+            alt={nfts[currentSlideIndex]?.name}
             className="object-contain h-[676px] w-[676px] mt-[-40px]"
           />
           <div className="flex justify-between px-4 py-2 absolute bottom-2 left-0 right-0 text-[#a3a3a3] w-full">
