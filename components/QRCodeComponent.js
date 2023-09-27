29e3f1e089d5bf8ab3efc61ec9a1c073fd56f511
toyop-GsF9 <a3saki4yume@gmail.com>
2023-09-27 11:08:37 +0900

    QRコード色変更

--- components/QRCodeComponent.js
+++ components/QRCodeComponent.js
@@ -5,7 +5,7 @@ import QRCode from 'qrcode.react';
 function QRCodeComponent({ value, size = 81 }) {
 	return (
 		<div className="w-[89px] h-[89px] bg-white p-1 z-10 ">
-			<QRCode value={value} fgColor="#0067c0" size={size} className="top-0 w-[81px] h-[81px]" />
+			<QRCode value={value} fgColor="#000000" size={size} className="top-0 w-[81px] h-[81px]" />
 		</div>
 	);
 }
