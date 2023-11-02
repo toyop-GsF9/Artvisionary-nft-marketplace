fefa535463a2c742b45b0c534eb972fbe552aa4b
toyop-GsF9 <a3saki4yume@gmail.com>
2023-11-03 06:54:00 +0900

    ショー変更

--- pages/square3.js
+++ pages/square3.js
@@ -33,7 +33,9 @@ export default function Square() {
     return contract;
   };
 
-  const getNfts = async () => {
+  const RETRY_INTERVAL = 5000; // 5 seconds
+  const MAX_RETRIES = 3;
+  const getNfts = async (retryCount = 0) => {
     try {
       const contract = await getContract();
 
@@ -64,8 +66,18 @@ export default function Square() {
       setLoading(true);
     } catch (error) {
       console.error(error);
-      toast.error("Something went wrong");
+
+      if (error.code === "CALL_EXCEPTION" && retryCount < MAX_RETRIES) {
+        // Retry after a delay
+        setTimeout(() => {
+          getNfts(retryCount + 1);
+        }, RETRY_INTERVAL);
+        return;
+      }
+
+      toast.error("Something went wrong、更新ボタンを押してください。");
     }
+
   };
 
 
@@ -84,7 +96,7 @@ export default function Square() {
     // 3秒ごとにnextSlide関数を呼び出すタイマーを設定
     const timerId = setInterval(() => {
       nextSlide();
-    }, 5000); // 3000ミリ秒 = 3秒
+    }, 10000); // 3000ミリ秒 = 3秒
 
     // useEffectのクリーンアップ関数でタイマーをクリア
     return () => {
@@ -96,11 +108,13 @@ export default function Square() {
 
   if (!loading)
     return (
-      <div className="w-full h-screen flex flex-col items-center justify-center font-body">
-        <img src="/logo.png" alt="logo" className="h-[160px] animate-bounce" />
-        <h2 className="text-7xl font-semibold ">Loading...</h2>
+      <div className="w-full h-screen flex flex-col items-center justify-center font-body bg-gradient-to-br from-gray-900 to-black">
+        <img src="/logo.png" alt="logo" className="h-[160px] animate-pulse" />
+        <h2 className="text-7xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-black mt-5">
+          Loading...
+        </h2>
       </div>
-    );
+    )
 
 
 
@@ -118,47 +132,52 @@ export default function Square() {
   const nftDetailURL = `https://artvisionary02.vercel.app/nft-details?price=${nfts[currentSlideIndex]?.price}&tokenId=${nfts[currentSlideIndex]?.tokenId}&seller=${nfts[currentSlideIndex]?.seller}&owner=${nfts[currentSlideIndex]?.owner}&image=${encodeURIComponent(nfts[currentSlideIndex]?.image)}&name=${encodeURIComponent(nfts[currentSlideIndex]?.name)}&description=${encodeURIComponent(nfts[currentSlideIndex]?.description)}&tokenURI=${encodeURIComponent(nfts[currentSlideIndex]?.tokenURI)}`;
   const nftDetailURL2 = `https://artvisionary02.vercel.app/dashboard`;
 
-
+  const truncateSeller = (seller) => seller.slice(-4);
   return (
     <>
       <Head>
         <title>Playlist（square） || Treasure Art</title>
         <link rel="shortcut icon" href="logo.png" />
       </Head>
-      {/* <div className="relative w-full max-w-[1440px] max-h-[930px] flex items-center justify-center text-left text-sm text-gray-600 bg-white"> */}
-      <div className="relative w-screen h-screen flex items-center justify-center text-left text-sm text-gray-600 bg-white overflow-hidden">
-        <div className="bg-black absolute top-0 left-0 w-full h-full"></div>
-
-        <div className="relative bg-[#1a1a1a] flex items-center justify-center border-4 border-gray-300 box-border w-11/12 max-w-[792px] p-[66px]">
+      <div className="flex items-center justify-center bg-black relative h-screen w-screen">
+        <div className="flex items-center justify-center relative bg-[#1a1a1a] h-full w-full">
           <img
             src={mainURL + nfts[currentSlideIndex]?.image}
             alt={nfts[currentSlideIndex]?.name}
-            className="object-contain w-full max-h-[75vh]"
+            className="object-contain h-full max-h-[90vh] w-full max-w-[90vw] p-4"
           />
-          <div className="flex justify-between px-4 py-2 absolute bottom-0 left-0 right-0 bg-[#1a1a1a] bg-opacity-40 text-white w-full">
+          <div className="flex justify-between absolute bottom-0 left-0 right-0 px-4 py-2 text-[#a3a3a3] w-full">
             <div className="flex items-center">
-              <img className="w-9 h-9 object-cover mr-1" alt="icon" src="/images/mask-group@2x.png" />
-              <span className="underline truncate">shin tanaka</span>
+              <img
+                className="w-9 h-9 object-cover mr-1"
+                alt="icon"
+                src="/images/mask-group@2x.png"
+              />
+              <span className="underline truncate ml-2">{truncateSeller(nfts[currentSlideIndex]?.seller)}</span>
+              <span className="truncate ml-6">{nfts[currentSlideIndex]?.name}</span>
             </div>
-            {/* <div className="font-semibold truncate">Point</div> */}
             <div className="flex items-center">
-              <img className="w-9 h-9 rounded-[9px]" alt="MATIC" src="/images/ethereum-1@2x.png" />
-              <span className="ml-2 font-semibold leading-[36px]">0.01 MATIC</span>
+              <img
+                className="w-8 h-6 rounded-[9px]"
+                alt="MATIC"
+                src="/images/polygonlogo-mono.png"
+              />
+              <span className="ml-2 font-semibold leading-[32px]">
+                {nfts[currentSlideIndex]?.price} MATIC
+              </span>
             </div>
           </div>
-        </div>
-        <div className="absolute bottom-0 ">
-          <QRCodeComponent value={nftDetailURL} />
-
+          <div className="absolute bottom-0 -mb-28 left-1/2 transform -translate-x-1/2">
+            <QRCodeComponent
+              value={nftDetailURL}
+              className="w-[81px] h-[81px]"
+            />
+          </div>
         </div>
       </div>
-      <style jsx>{`
-      @media (max-width: 640px) {
-        // Mobile styles here
-      }
-    `}</style>
     </>
   );
+
 
 
 }
