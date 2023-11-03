8d619ce62334dd6ded2537286e9b9a3b72f4493e
toyop-GsF9 <a3saki4yume@gmail.com>
2023-11-03 12:33:32 +0900

    デザイン変更

--- pages/2.js
+++ pages/2.js
@@ -27,16 +27,21 @@ export default function Home() {
       });
       setIsWalletConnected(true);
       localStorage.setItem("walletAddress", accounts[0]);
-      router.push("/dashboard");
+      router.push("/");
     } catch (error) {
       console.error(error);
     }
   };
 
+
   useEffect(() => {
     const addr = localStorage.getItem("walletAddress");
-    setAddr(addr);
+    if (addr) {
+      setIsWalletConnected(true);
+      setAddr(addr);
+    }
   }, []);
+
 
   const playlists = {
 
@@ -78,89 +83,95 @@ export default function Home() {
 
       <div className="relative overflow-hidden bg-black">
 
-        <section className="max-w-[1240px] mt-10 mb-20 mx-auto grid grid-cols-2  gap-2 font-body h-[930px] overflow-hidden top-7 md:gap-12 medium md:px-5 sm:grid-cols-1 sm:h-full relative ">
-          <div className="relative flex flex-col items-center justify-center h-full min-h-[56vh] sm:items-center">
+        <section className="max-w-[1240px]  mb-10 mx-auto grid grid-cols-3 gap-2 font-body h-[930px] overflow-hidden top-7 md:gap-12 medium md:px-1 lg:grid-cols-1 sm:h-full relative">
+
+
+
+          <div className="relative flex flex-col items-center justify-center h-full min-h-[56vh] sm:items-center z-10">
 
             <div className="absolute top-0 left-0 w-full h-[0px] pb-full">
-              <img src="/images/heroimage3.jpeg" className="absolute top-0 left-0 w-full  object-cover" alt="Background Image" />
+              <img src="/images/heroimage3.jpeg" className="absolute top-0 left-0 w-[581px]  object-cover h-[581px] " alt="Background Image" />
             </div>
-
-
-            <div className="absolute top-1/4 left-0 w-full h-1/4 transform -translate-y-1/2">
-              <img src="/images/Treasure Art_LOGO_L.png" className="w-full  object-cover" alt="Foreground Image" />
-            </div>
-
-            <div className="absolute top-[calc(30%+12.5%)] left-[15px] w-full">
+            {/* <div className="absolute top-[calc(30%+12.5%)] left-[15px] w-full"> */}
+            <div className="absolute  top-[calc(30%+12.5%)] lg:top-[calc(50%+12.5%)] md:top-[calc(80%+12.5%)] sm:top-[calc(140%+12.5%)] left-[15px] w-full">
               {addr ? (
                 <button
                   type="button"
-                  className="bg-[#1E50FF] outline-none border-none py-3 px-5 rounded-xl font-body cursor-pointer transition duration-250 ease-in-out hover:scale-125 hover:drop-shadow-xl hover:shadow-sky-600 w-auto focus:scale-90 z-20"
-                  onClick={connectWallet}
+                  className="bg-[#1E50FF] text-white outline-none border-none py-3 px-5 rounded-xl font-body cursor-pointer transition duration-250 ease-in-out hover:scale-125 hover:drop-shadow-xl hover:shadow-sky-600 w-auto focus:scale-90 z-100"
+
+                  onClick={() => {
+
+                    window.location.href = "/createnft";
+                  }}
                 >
                   Create an NFT
                 </button>
               ) : (
                 <button
                   type="button"
-                  className="bg-[#1E50FF] outline-none border-none py-3 px-5 rounded-xl font-body cursor-pointer duration-250 ease-in-out hover:transform-x-1 hover:drop-shadow-xl hover:shadow-sky-600 w-auto transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none z-20"
+                  className="bg-[#1E50FF] text-white  outline-none border-none py-3 px-5 rounded-xl font-body cursor-pointer duration-250 ease-in-out hover:transform-x-1 hover:drop-shadow-xl hover:shadow-sky-600 w-auto transition transform hover:-translate-y-3 motion-reduce:transition-none motion-reduce:hover:transform-none z-100"
                   onClick={connectWallet}
                 >
-                  Connect Wallet
+                  Wallet接続
                 </button>
               )}
             </div>
-            <p className="absolute bottom-[15%] left-1/2 transform -translate-x-1/2 text-white text-lg w-[70%] text-center">
-              「育つ財宝」を探し出すアートマーケットプレイス！
-              あなたの目が選んだアートピースがショーケースに並びます。
-              ディスプレイして鑑賞するだけでなく、売ることもできる。
-              自分だけのコレクションにもできる。
-              アートを楽しめば楽しむほど、アーチストの応援になります。
+          </div>
+
+          {/* <div className="flex flex-col items-center mt-20 ml-[-14rem] w-[581px] md:w-[80%] lg:w-[60%]"> */}
+          <div className="flex flex-col items-center mt-20 lg:mt-[-190px] md:mt-36  ml-[-14rem] w-[581px] md:w-[581px] lg:w-[581px] sm:w-full z-30 transform transition-transform duration-500 lg:translate-y-[-68%] md:translate-y-[-80%] sm:translate-y-[-120%] lg:ml-0 md:ml-0 sm:ml-0">
+            <img src="/images/Treasure Art_LOGO_L.png" className="w-full h-auto z-10" alt="Foreground Image" />
+            <p className="mt-72 text-white text-lg md:text-xs  text-center z-10 lg:mt-64 md:mt-64 sm:top-60">
+              「育つ財宝」を探し出すアートマーケットプレイス！<br />
+              あなたの目が選んだアートピースがショーケースに並びます。<br />
+              ディスプレイして鑑賞するだけでなく、売ることもできる。<br />
+              自分だけのコレクションにもできる。<br />
+              アートを楽しめば楽しむほど、アーチストの応援になります。<br />
             </p>
 
           </div>
 
-          <div className="w-full flex  justify-center ">
-            <div className="w-[350px] h-[480px] bg-[#272D37]/60 rounded-2xl flex flex-col p-6 sm:h-max mt-44">
+
+          <div className="flex justify-center">
+
+            <div className="w-[350px] h-[480px] bg-[#272D37]/60 rounded-2xl flex flex-col p-6 sm:h-max mt-40  sm:mt-[-10rem] sm:mb-4">
               <Slideshow2 playlists={playlists} width="300" height="300" />
               <div className="">
-                <h1>example</h1>
+                <h1 className="text-[#ADB9C7]">example</h1>
                 <div className="h-[56px] flex justify-between">
                   <div className="flex flex-row gap-2">
                     <img
                       src="images/mockcreator.jpg"
                       alt="creator-image"
-                      className="h-[56px] w-[56px] rounded-xl"
+                      className="h-[36px] w-[36px] rounded-xl"
                     />
                     <div>
                       <p className="my-1 text-base text-[#8F9CA9]">Creator </p>
-                      <h4 className="my-0">0x000...0000</h4>
+                      <h4 className="my-0 text-[#ADB9C7]">0x00...</h4>
                     </div>
                   </div>
                   <div>
-                    <p className="my-1 text-[#8F9CA9]">Current Price</p>
-                    <h4 className="my-0 ">4.99 ETH</h4>
+                    <p className="my-1 text-[#8F9CA9]">Price</p>
+                    <h4 className="my-0 text-[#ADB9C7]">1.0 MATIC</h4>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
         </section>
-        {/* 
-        <section className="max-w-[1240px] mt-10 mb-20 mx-auto grid grid-cols-3 gap-2 font-body h-[930px] overflow-hidden top-7 md:gap-12 medium md:px-5 sm:grid-cols-1 sm:h-full relative ">
 
-        
+
 
 
         {/* <section className="max-w-[1240px] mt-[-7rem] mb-20 mx-auto  gap-2 font-body top-7 "> */}
         <section className="max-w-[1240px] mt-10 mb-20 mx-auto  gap-2 font-body top-7 ">
           <h1 className="text-center w-full text-[#fff]">Create and sell your NFTs</h1>
 
-          <div className="top-7 grid grid-cols-3 gap-5 sm:grid-cols-1 sm:p-12 md:grid-cols-1 md:mx-10">
+          <div className="top-7 grid grid-cols-3 gap-5 sm:grid-cols-1 sm:p-0 md:grid-cols-1 md:mx-10">
             {data.map((item) => (
               <div
                 key={item.id}
-                className="w-full bg-[#52565e] flex flex-col justify-center items-center p-3 rounded-xl"
+                className="w-full bg-[#272D3799] flex flex-col justify-center items-center p-3 rounded-xl"
               >
                 <div className="w-[80px] h-[80px] flex  justify-center items-center ">
                   <img
@@ -169,7 +180,7 @@ export default function Home() {
                     className="w-full h-full"
                   />
                 </div>
-                <h4 className="font-bold text-[24px] md:text-[14px]">
+                <h4 className="font-bold text-[24px] text-[#ADB9C7] md:text-[14px]">
                   {item.title}
                 </h4>
                 <p className="text-center text-[#ADB9C7] text-[14px]">
@@ -183,10 +194,10 @@ export default function Home() {
         <section className="max-w-[1240px] my-20 mx-auto  gap-2 font-body top-7 ">
           <h1 className="text-center w-full text-[#fff]">Show case</h1>
 
-          <div className="grid grid-cols-4 gap-3 sm:gap-y-8 md:grid-cols-2 sm:grid-cols-1 sm:p-12 md:mx-10">
+          <div className="grid grid-cols-4 gap-3 sm:gap-y-8 md:grid-cols-2 sm:grid-cols-1 sm:p-0 md:mx-10">
             {mockartist.map((data) => (
               <Link href={`/${data.name}`} key={data.id}>
-                <a className="w-full bg-[#52565e] flex flex-col justify-center items-center p-3 rounded-xl">
+                <a className="w-full bg-[#272D3799] flex flex-col justify-center items-center p-3 rounded-xl">
                   <div className="w-full relative" style={{ paddingBottom: '100%' }}>
                     <img
                       src={data.bgImage}
@@ -195,7 +206,7 @@ export default function Home() {
                       className="absolute w-full h-full rounded-2xl object-fit: contain"
                     />
                   </div>
-                  <div className="w-full text-center mt-8 font-bold">
+                  <div className="w-full text-center mt-8 font-bold text-[#ADB9C7]">
                     <h3>{data.name}</h3>
                   </div>
                 </a>
@@ -207,9 +218,9 @@ export default function Home() {
 
 
         {/* Community */}
-        <section className="max-w-[1240px] bg- my-20 mx-auto bg-[#52565e] gap-2 font-body top-7 text-center rounded-xl sm:mx-10 md:m-10 border border-solid border-sky-600">
+        <section className="max-w-[1240px] bg- my-20 mx-auto bg-[#272D3799] gap-2 font-body top-7 text-center rounded-xl sm:mx-10 md:m-10 border border-solid border-sky-600">
           <div>
-            <h1 className="text-4xl sm:text-2xl">Create Your Own NFT!</h1>
+            <h1 className="text-2xl sm:text-2xl text-[#ADB9C7]">Create Your Own NFT!</h1>
             {/* <p className="text-[#ADB9C7] px-[120px] sm:p-2 ms:p-1">
               We have a large scale group to support each other in this game,
               Join us to get the news as soon as possible and follow our latest
